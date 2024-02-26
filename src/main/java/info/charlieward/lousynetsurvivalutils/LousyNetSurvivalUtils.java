09724805f364e55350e5e26206d76f81d4595d20
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-26 11:48:22 +0000

    Initial Setup

--- src/main/java/info/charlieward/lousynetsurvivalutils/LousyNetSurvivalUtils.java
+++ src/main/java/info/charlieward/lousynetsurvivalutils/LousyNetSurvivalUtils.java
@@ -1,17 +1,26 @@
 package info.charlieward.lousynetsurvivalutils;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import redis.clients.jedis.Jedis;
 
 public final class LousyNetSurvivalUtils extends JavaPlugin {
 
+    private static LousyNetSurvivalUtils plugin;
+    public Jedis jedis = new Jedis();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        getLogger().info("LousyNet-SurvivalUtils v." + this.getDescription().getVersion() + " has loaded.");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("LousyNet-SurvivalUtils v." + this.getDescription().getVersion() + " has been disabled.");
+    }
+
+    public static LousyNetSurvivalUtils getPlugin() {
+        return plugin;
     }
 }
