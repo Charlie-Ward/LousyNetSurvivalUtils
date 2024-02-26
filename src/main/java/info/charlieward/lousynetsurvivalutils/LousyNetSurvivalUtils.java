bf28a8a771439d0103c5fd1c3864cb745c9733dd
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-26 10:46:07 +0000

    Initial commit

---
+++ src/main/java/info/charlieward/lousynetsurvivalutils/LousyNetSurvivalUtils.java
@@ -0,0 +1,17 @@
+package info.charlieward.lousynetsurvivalutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LousyNetSurvivalUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
